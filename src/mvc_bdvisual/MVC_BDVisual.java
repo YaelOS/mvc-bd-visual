5856e5b387ca55a897a10814e95dbd3a230b346f
Yael Otamendi Salazar <yaelos03@micorreo.upp.edu.mx>
2022-02-14 08:05:16 -0600

    Se han guardado todos los archivos del repositorio

---
+++ src/mvc_bdvisual/MVC_BDVisual.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc_bdvisual;
+
+/**
+ *
+ * @author 980013684
+ */
+public class MVC_BDVisual {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
